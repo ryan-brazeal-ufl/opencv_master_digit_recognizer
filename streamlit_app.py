fc9d87c0f5764f0b73c4cf1aff9703888ce0b54a
ryan_brazeal <ryan.brazeal@sasktel.net>
2026-02-06 14:06:07 -0600

    try to launch the Flask app on a thread

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import threading
+from cv_web_app import app as flask_app
+
+def run_flask():
+    flask_app.run(port=5000, use_reloader=False)
+
+threading.Thread(target=run_flask, daemon=True).start()
 
 st.components.v1.iframe(
     "http://localhost:5000",
