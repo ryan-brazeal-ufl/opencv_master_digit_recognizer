6b82e1d544b4f46c4159ee174a6b4c17567bcc8d
ryan_brazeal <ryan.brazeal@sasktel.net>
2026-02-06 13:49:36 -0600

    revert change host and port

--- cv_web_app.py
+++ cv_web_app.py
@@ -63,4 +63,4 @@ def upload():
   return json.dumps({'success':True, 'class': int(classId), 'confidence': float(confidence)}), 200, {'ContentType':'application/json'} 
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=80)
+   app.run()
